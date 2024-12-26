3314d27f9d201d45a64ddc4e8b46c42c75c9f750
fuad71 <fuadul202@gmail.com>
2024-12-26 18:52:19 +0600

    transaction is completly done

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,7 +2,6 @@ package sqlc
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,18 +13,15 @@ func TestTransgerTx(t *testing.T) {
 	account1 := CreateRandomAccount(t)
 	account2 := CreateRandomAccount(t)
 
-	fmt.Println(">> before: ", account1.Balance, account2.Balance)
-	n := 2
+	n := 5
 	amount := int64(10)
 
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
-		txName := fmt.Sprintf("tx %d", i+1)
 		go func() {
-			ctx := context.WithValue(context.Background(), txKey, txName)
-			result, err := store.TransferTx(ctx, TransferTxParams{
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
@@ -86,8 +82,6 @@ func TestTransgerTx(t *testing.T) {
 		require.Equal(t, account2.ID, toAccount.ID)
 
 		// TODO: check accounts' balance
-
-		fmt.Println(">> before: ", fromAccount.Balance, toAccount.Balance)
 		diff1 := account1.Balance - fromAccount.Balance
 		diff2 := toAccount.Balance - account2.Balance
 
@@ -109,7 +103,6 @@ func TestTransgerTx(t *testing.T) {
 	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
 	require.NoError(t, err)
 
-	fmt.Println(">> before: ", updatedAccount1.Balance, updatedAccount2.Balance)
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, updatedAccount2.Balance, account2.Balance+int64(n)*amount)
 
