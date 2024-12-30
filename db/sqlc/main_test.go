aa574944cafda9e8f4f89885a3b51bde5177682e
fuad71 <fuadul202@gmail.com>
2024-12-30 12:11:07 +0600

    env file added

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,16 @@ import (
 	"os"
 	"testing"
 
+	"github.com/fuad7161/simple-bank/db/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
--- main.go
+++ main.go
@@ -6,24 +6,21 @@ import (
 
 	"github.com/fuad7161/simple-bank/api"
 	db "github.com/fuad7161/simple-bank/db/sqlc"
+	"github.com/fuad7161/simple-bank/db/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig(".")
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("connot start server: ", err)
 	}
